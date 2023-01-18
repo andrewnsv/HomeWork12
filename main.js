65d11c2f70abeb69ce04091227d8df2acb3eda29
andrewnsv <andrejnizovoj598@gmail.com>
2023-01-18 23:14:24 +0200

    add homework

---
+++ main.js
@@ -0,0 +1,67 @@
+const redLight = document.querySelector(".red");
+const yellowLight = document.querySelector(".yellow");
+const greenLight = document.querySelector(".green");
+const walkBtn = document.getElementById("walk");
+const counter = document.querySelector(".counter");
+
+let timeLeft = 3;
+let currentLight = "red";
+redLight.style.backgroundColor = "red";
+yellowLight.style.backgroundColor = "black";
+greenLight.style.backgroundColor = "black";
+
+
+let changeLight = setInterval(() => {
+  if (currentLight === "red") {
+    redLight.style.backgroundColor = "black";
+    yellowLight.style.backgroundColor = "yellow";
+    currentLight = "yellow";
+  } else if (currentLight === "yellow") {
+    yellowLight.style.backgroundColor = "black";
+    greenLight.style.backgroundColor = "green";
+    currentLight = "green";
+  } else {
+    greenLight.style.backgroundColor = "black";
+    redLight.style.backgroundColor = "red";
+    currentLight = "red";
+  }
+}, 1000);
+
+walkBtn.addEventListener("click", () => {
+  clearInterval(changeLight);
+  
+  let currentLight = "red";
+  redLight.style.backgroundColor = "red";
+  yellowLight.style.backgroundColor = "black";
+  greenLight.style.backgroundColor = "black";
+  counter.textContent = `Times left: ${timeLeft}`;
+
+  setTimeout(() => {
+    return changeLight
+  }, 1000);
+
+  // const updateCounter = () => {
+  //   timeLeft--;
+  //   counter.textContent = `Times left: ${timeLeft}`;
+  //   if (timeLeft === 0) {
+  //     changeLight = setInterval(() => {
+  //       if (currentLight === "red") {
+  //         redLight.style.backgroundColor = "black";
+  //         yellowLight.style.backgroundColor = "yellow";
+  //         currentLight = "yellow";
+  //       } else if (currentLight === "yellow") {
+  //         yellowLight.style.backgroundColor = "black";
+  //         greenLight.style.backgroundColor = "green";
+  //         currentLight = "green";
+  //       } else {
+  //         greenLight.style.backgroundColor = "black";
+  //         redLight.style.backgroundColor = "red";
+  //         currentLight = "red";
+  //       }
+  //     }, 1000);
+  //   } else {
+  //     counterTimeout = setTimeout(updateCounter, 1000);
+  //   }
+  // };
+  // counterTimeout = setTimeout(updateCounter, 1000);
+});
